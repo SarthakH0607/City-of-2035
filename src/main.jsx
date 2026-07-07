04faa736c4dd10d5c647ec3fd4b4eff2b4b7ce30
SarthakH0607 <hundaresarthak@gmail.com>
2026-07-08 00:13:09 +0530

    Configure router basename for subpath hosting

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-﻿import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
+const basename = import.meta.env.BASE_URL;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
